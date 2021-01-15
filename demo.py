a3c08a7db1eb2aaf924dd48850f05862d7cd3841
icacedo <icacedo@ucdavis.edu>
2021-01-15 10:18:53 -0800

    in progress assignment 1

---
+++ demo.py
@@ -0,0 +1,7 @@
+import argparse # provided by python
+import sys # probided by python
+
+import seqlib # this is seqlib.py that was created and put in this directory
+
+parser = argparse.ArgumentParser(
+	description='Count k-mers in a sequence')
