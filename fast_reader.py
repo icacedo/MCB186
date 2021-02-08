9f65b8539885a8698306f83f0dbccc276d2b0d6d
icacedo <icacedo@ucdavis.edu>
2021-02-08 11:20:15 -0800

    working version of fast_reader.py

--- fast_reader.py
+++ fast_reader.py
@@ -1,4 +1,6 @@
 import seqlib
+import statistics 
+import sys
 
 # testing a list of lists
 #list1 = ['se', 'qu', 'en', 'ce']
@@ -11,6 +13,7 @@ import seqlib
 
 all_probs = []
 for name, seq, qual in seqlib.read_fastq('mini.fastq.gz'):
+	qual = qual.rstrip()
 	probs = []
 	# this loop goes by each position in a single string of quality...
 	# ...values for one sequence
@@ -22,13 +25,36 @@ for name, seq, qual in seqlib.read_fastq('mini.fastq.gz'):
 
 # sum and get the averages for the values in all_probs
 # sum each 'column' corresponding to the same position in each sequence
-# there are 100000 sequences
+# this loop is for the total number of sequences
+
+
+'''
+avgs = []
 for q1 in range(len(all_probs)):
-	print(all_probs[q1])
-	sys.exit()
-	#for q2 in all_probs[q1]:
-	# there are 152 positions in each sequence
-		#print(all_probs[q2])
+	# this loop is for number of positions in each sequence
+	total_p = 0
+	seq_avg = []
+	for q2 in all_probs[q1]:
+		total_p += q2
+		seq_avg.append(total_p / len(all_probs[q1]))
+		print(q2)
+		sys.exit()
+	avgs.append(seq_avg)
+print(len(all_probs))
+print(len(avgs))
+print(len(seq_avg))
+'''
+
+
+for i in range(len(probs)):
+	column = []
+	for row in all_probs:
+		column.append(row[i])
+	print(i, statistics.mean(column))
+	
+	
+
+
 
 
 
